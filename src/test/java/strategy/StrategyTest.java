a21b093aff0fe4fddb86433b80667be600a64c26
Christophe Maldivi <christophe.maldivi@orange.com>
2017-10-01 22:04:44 +0200

    chore(editor): add editor config, main, etc

--- src/test/java/strategy/StrategyTest.java
+++ src/test/java/strategy/StrategyTest.java
@@ -7,6 +7,11 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 
+/**
+ * Define a family of algorithms, encapsulate each one, and make them interchangeable.
+ * Strategy lets the algorithm vary independently from clients that use it.
+ */
+
 public class StrategyTest {
 
   String consoleMessage = "";
