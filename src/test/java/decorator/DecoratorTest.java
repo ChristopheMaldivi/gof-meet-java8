f2b0bd2357cdee872f2c2a7621fbb8dbe44b47b0
Christophe Maldivi <christophe.maldivi@orange.com>
2017-10-02 22:12:42 +0200

    fix(decorator): fix decorator and add M. Fusco 's GOF examples

--- src/test/java/decorator/DecoratorTest.java
+++ src/test/java/decorator/DecoratorTest.java
@@ -3,6 +3,8 @@ package decorator;
 import org.fest.assertions.Delta;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.DoubleUnaryOperator;
 import java.util.stream.Stream;
 
@@ -18,11 +20,11 @@ public class DecoratorTest {
   @Test
   public void compute_salary_per_month() {
     // given
-    float rawSalary = 30000;
+    float annualSalary = 30000;
     DoubleUnaryOperator computeSalaryPerMonth = salary -> salary / 12;
 
     // when
-    double salaryPerMonth = computeSalaryPerMonth.applyAsDouble(rawSalary);
+    double salaryPerMonth = computeSalaryPerMonth.applyAsDouble(annualSalary);
 
     // then
     assertThat(salaryPerMonth).isEqualTo(2500, Delta.delta(0.001f));
@@ -31,7 +33,7 @@ public class DecoratorTest {
   @Test
   public void general_tax_then_regional_tax_then_health_insurance_are_applied_on_salary() {
     // given
-    float rawSalary = 30000;
+    float annualSalary = 30000;
 
     DoubleUnaryOperator computeSalaryPerMonth = salary -> salary / 12;
     DoubleUnaryOperator lessGeneralTax = salary -> salary * 0.8;
@@ -44,16 +46,45 @@ public class DecoratorTest {
         .andThen(lessHealthInsurance);
 
     // when
-    double salaryPerMonthLessTaxes = lessTaxes.applyAsDouble(rawSalary);
+    double salaryPerMonthLessTaxes = lessTaxes.applyAsDouble(annualSalary);
 
     // then
     assertThat(salaryPerMonthLessTaxes).isEqualTo(1700f, Delta.delta(0.001f));
   }
 
+
+  class MutableDouble {
+    double d;
+    public MutableDouble(double d) {
+      this.d = d;
+    }
+  }
+
   @Test
   public void general_tax_then_regional_tax_then_health_insurance_are_applied_on_salary2() {
     // given
-    float rawPrice = 30000;
+    double annualSalary = 30000;
+
+    DoubleUnaryOperator computeSalaryPerMonth = salary -> salary / 12;
+    DoubleUnaryOperator lessGeneralTax = salary -> salary * 0.8;
+    DoubleUnaryOperator lessRegionalTax = salary -> salary * 0.95;
+    DoubleUnaryOperator lessHealthInsurance = salary -> salary - 200;
+    List<DoubleUnaryOperator> taxes = Arrays.asList(new DoubleUnaryOperator[]{
+      computeSalaryPerMonth, lessGeneralTax, lessRegionalTax, lessHealthInsurance
+    });
+
+    // when
+    final MutableDouble salaryPerMonthLessTaxes = new MutableDouble(annualSalary);
+    taxes.forEach(f -> salaryPerMonthLessTaxes.d = f.applyAsDouble(salaryPerMonthLessTaxes.d));
+
+    // then
+    assertThat(salaryPerMonthLessTaxes.d).isEqualTo(1700, Delta.delta(0.001f));
+  }
+
+  @Test
+  public void general_tax_then_regional_tax_then_health_insurance_are_applied_on_salary3() {
+    // given
+    float annualSalary = 30000;
 
     DoubleUnaryOperator computeSalaryPerMonth = salary -> salary / 12;
     DoubleUnaryOperator lessGeneralTax = salary -> salary * 0.8;
@@ -63,11 +94,11 @@ public class DecoratorTest {
       Stream.of(computeSalaryPerMonth, lessGeneralTax, lessRegionalTax, lessHealthInsurance);
 
     // when
-    double salaryWithTaxes = taxes.reduce(
+    double salaryPerMonthLessTaxes = taxes.reduce(
         DoubleUnaryOperator.identity(), DoubleUnaryOperator::andThen)
-      .applyAsDouble(rawPrice);
+      .applyAsDouble(annualSalary);
 
     // then
-    assertThat(salaryWithTaxes).isEqualTo(1700, Delta.delta(0.001f));
+    assertThat(salaryPerMonthLessTaxes).isEqualTo(1700, Delta.delta(0.001f));
   }
 }
